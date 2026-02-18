9eb42540b288dfcc7ec97295bbc512f70caa7a01
Mazaiaulfa <mazaiaulfa123@gmail.com>
2026-02-18 09:32:23 +0700

    input file

--- resources/views/admin/documents/create.blade.php
+++ resources/views/admin/documents/create.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="max-w-xl">
-        <h1 class="text-xl font-bold mb-4">Tambah Dokumen</h1>
+    <div class="max-w-4xl mx-auto">
 
+    <div class="bg-white rounded-2xl shadow-xl overflow-hidden">
+
+        {{-- Header --}}
+        <div class="bg-gradient-to-r from-indigo-600 to-purple-600 px-6 py-5">
+            <h2 class="text-white text-xl font-semibold">
+                Tambah User
+            </h2>
+            <p class="text-indigo-100 text-sm">
+                Form pembuatan Dokument baru
+            </p>
+        </div>
+
+        {{-- Body --}}
+        <div class="p-8">
         <form method="POST" action="{{ route('admin.documents.store') }}">
             @csrf
 
@@ -47,6 +60,10 @@
             <div class="mb-3">
                 <label>Revisi</label>
                 <input type="text" name="revision" class="w-full border px-3 py-2">
+            </div>
+               <div class="mb-3">
+                <label>File</label>
+                <input type="file" name="file_document" class="w-full border px-3 py-2">
             </div>
 
             <div class="mb-3">
--- resources/views/admin/documents/index.blade.php
+++ resources/views/admin/documents/index.blade.php
@@ -1,14 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <h1 class="text-xl font-bold mb-4">Daftar Dokumen</h1>
+<div class="bg-indigo-600 text-white px-6 py-3 rounded-t-xl">
+    <h2 class="font-semibold text-lg">Data Dokumen</h2>
+</div>
 
-    <a href="{{ route('admin.documents.create') }}"
+<div class="bg-white rounded-b-xl shadow border border-gray-100 p-5">
+    <div class="flex justify-end mb-4">
+
+     <a href="{{ route('admin.documents.create') }}"
        class="bg-blue-500 text-white px-4 py-2 rounded">
-        + Tambah Dokumen
+        Tambah Dokumen
     </a>
+</div>
 
+<div class="bg-white rounded-b-xl shadow border border-gray-100 p-5">
+    <div class="flex justify-between items-center mb-4">
+
+        <div class="text-sm text-gray-600">
+            Show
+            <select class="border border-gray-300 rounded px-2 py-1 text-sm mx-1">
+                <option>0</option>
+                <option>10</option>
+                <option>20</option>
+            </select>
+            entries
+        </div>
+
+        <!-- search -->
+        <div class="flex border border-gray-300 rounded-lg overflow-hidden">
+            <input
+                type="text"
+                placeholder="Search..."
+                class="px-3 py-1 text-sm focus:outline-none">
+            <button class="px-3 bg-indigo-500 text-white text-sm">
+                Cari
+            </button>
+        </div>
+
+    </div>
+
+    <!-- ================= TABLE ================= -->
+    <div class="overflow-x-auto">
     <table class="w-full mt-4 border">
         <thead>
             <tr class="bg-gray-100">
@@ -18,6 +51,7 @@
                 <th class="border p-2">Departemen</th>
                 <th class="border p-2">Nomor Dokumen</th>
                 <th class="border p-2">Revisi</th>
+                <th class="border p-2">File</th>
                 <th class="border p-2">Tanggal</th>
                 <th class="border p-2">Keterangan</th>
             </tr>
@@ -31,6 +65,7 @@
                     <td class="border p-2">{{ $doc->department->name }}</td>
                     <td class="border p-2">{{ $doc->document_number }}</td>
                     <td class="border p-2">{{ $doc->revision }}</td>
+                    <td class="border p-2">Kosong</td>
                     <td class="border p-2">{{ $doc->document_date }}</td>
                     <td class="border p-2">{{ $doc->description }}</td>
                 </tr>
